fe0a35300fd345d79b4fe45770c56729b1f92052
Aditya Dahake <dahakeaditya263@gmail.com>
2025-11-23 22:53:01 +0530

    Backend Created to for food order - Aditya Dahake

---
+++ backend/src/routes/food.routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const foodController = require('../controllers/food.controller');
+const authMiddleware = require('../middleware/auth.middleware');
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+});
+const router = express.Router();
+
+
+// food routes
+router.post('/', 
+    authMiddleware.authFoodPartnerMiddleware, 
+    upload.single('video'), 
+    foodController.createFood);
+
+
+
+module.exports = router;
